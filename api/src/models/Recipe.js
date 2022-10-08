acc73185612b2d62fc694c2693400c069c190a43
Lafsitoo <fretteleandro@live.com>
2022-10-08 03:27:44 -0300

    8/10-3:27

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,41 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "recipe",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        defaultValue:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/table-filled-with-large-variety-of-food-shot-from-royalty-free-image-1659038707.jpg?crop=0.668xw:1.00xh;0.167xw,0&resize=640:*",
+      },
+      tipoDelPlato: {
+        type: DataTypes.STRING,
+      },
+      resumenDelPlato: {
+        type: DataTypes.STRING,
+      },
+      nivelSalubre: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      pasoAPaso: {
+        type: DataTypes.STRING,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
