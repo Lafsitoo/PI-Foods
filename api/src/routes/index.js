3b1d53ceffcf2e758c86a6a60ec5018b8964ad91
Lafsitoo <fretteleandro@live.com>
2022-10-11 04:32:06 -0300

    11/10-04:32

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -65,8 +65,8 @@ router.get("/recipes", async (req, res) => {
     const totalRecipes = await getAllRecipes();
 
     if (name) {
-      titleRecipe = totalRecipes.filter((e) =>
-        e.name.toLowerCase().includes(name.toLowerCase())
+      titleRecipe = totalRecipes.filter(
+        (e) => e.name.toLowerCase().includes(name.toLowerCase()) // ¿Incluye lo que pasa por query?
       );
       titleRecipe.length
         ? res.status(200).send(titleRecipe)
@@ -81,6 +81,38 @@ router.get("/recipes", async (req, res) => {
   } catch (error) {
     res.status(404).send(error);
     console.error(error);
+  }
+});
+
+//? Obtener todas las dietas posibles
+
+router.get("/diets", async (req, res) => {
+  const listDiets = [
+    "gluten free",
+    "dairy free",
+    "ketogenic",
+    "lacto ovo vegetarian",
+    "vegan",
+    "pescatarian",
+    "paleolithic",
+    "primal",
+    "fodmap friendly",
+    "whole 30",
+  ];
+  
+  try {
+    // Si lo que busco y ya esta dentro de mi tabla, no lo creo
+    listDiets.forEach((e) => {
+      Diet.findOrCreate({
+        where: { name: e },
+      });
+    });
+
+    const all = await Diet.findAll();
+    res.status(200).send(all);
+
+  } catch (error) {
+    res.status(404).send("Error");
   }
 });
 
